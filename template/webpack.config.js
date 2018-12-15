e3e44af967f76cb9dd8b01ae68a636ed9e0813bd
yanglu@thejoyrun.com <yanglu@thejoyrun.com>
2018-12-15 21:00:02 +0800

    update

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -7,19 +7,6 @@ var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/main.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '{{ name }}',
-      template: './src/index.html',
-      filename: path.resolve(__dirname, './index.html'),
-      inject: true,
-      alwaysWriteToDisk: false
-    }),
-    new HtmlWebpackHarddiskPlugin({
-      outputPath: path.resolve(__dirname, './index.html')
-    }),
-    new CleanWebpackPlugin(['dist'])
-  ],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: process.env.NODE_ENV === 'production' ? '/activity/{{ name }}/dist/' : '/dist/',
